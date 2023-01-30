80ad162471f152da3e405fca62100b954c6caa75
dina <Dinakaran0651@gmail.com>
2023-01-30 23:05:00 +0530

    update ui

--- script/script.js
+++ script/script.js
@@ -1,30 +1,8 @@
-//Calculate Tip
-
-function CalculateTip() {
-  //querrySelector() Method Returns The Element Value With Specified class
-    var billAmt =Document.querySelector(".BillAmt").Value;
-    var Servicequality = Document.querySelector(".Servicequality").Value;
-
-    var tipTotal = Document.querySelector(".TipTotal").Value;
-    var billTotal = Document.querySelector(".BillTotal").Value;
-
-  //Tip Calculation
-
-    var Total = billAmt * Servicequality; //For Tip Calculation
-
-    var Total = ParseFloat(billAmt) + ParseFloat(Total); //For Total Bill Amount Calculation
-
-  //Display  Tip
-
-    Document.querySelector(".tipTotal").InnerHTML = Total;
-
-  //Display Total
-
-    Document.querySelector(".billTotal").InnerHTML = Total;
-}
-
-//Click To Call Function
-
-Document.querySelector(".Calculate").Onclick = function (){
-    CalculateTip();
+const Bill = () => {
+	let billamount = document.getElementById("bamount").value;
+	let tipercentage = document.getElementById("tpercentage").value;
+	let tip = billamount * (tipercentage / 100);
+	let total = tip + Number(billamount);
+	document.getElementById("ttamount").value = tip;
+	document.getElementById("tbamount").value = total;
 };
